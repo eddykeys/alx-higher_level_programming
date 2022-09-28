fb58c5e085ce5bbb786d70f7497951149920c061
eddykeys <eddyonkeys@gmail.com>
2022-09-28 15:39:26 -0700

    new

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    l = list(my_string)
-    for x in l:
-        if x == 'c':
-            l.remove('c')
-        if x == 'C':
-            l.remove('C')
-    my_string = "".join(l)
-    return my_string
+    ret_str = [var for var in my_string if var.lower() != 'c']
+    return (''.join(ret_str))
